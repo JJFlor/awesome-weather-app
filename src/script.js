2bfdb8819f6d986594c210af40674e57e9e9ee15
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2020-12-03 17:43:40 +0100

    Added relevant icon API

--- src/script.js
+++ src/script.js
@@ -16,13 +16,14 @@
       }
 
       function showTemp(response) { 
-        console.log(response);
          document.querySelector("#searchedCity").innerHTML = response.data.name;
          document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
          document.querySelector("#humidity").innerHTML = response.data.main.humidity;
          document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-         document.querySelector("#currentWeather").innerHTML = response.data.weather[0].main;
+         document.querySelector("#currentWeather").innerHTML = response.data.weather[0].description;
          document.querySelector("#currentDate").innerHTML = formatDate(response.data.dt*1000);
+         document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+         document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
        }
 
       function searchCity (city){
