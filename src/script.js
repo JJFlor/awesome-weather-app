817c5109febceab11f8e0f2b20aa4ba738821222
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2020-12-04 17:22:41 +0100

    Added JS: 5 Days Forecast

--- src/script.js
+++ src/script.js
@@ -1,18 +1,10 @@
 
       function formatDate (timestamp) {
         let date = new Date(timestamp);
-        let hours = date.getHours();
-          if (hours < 10) {
-             hours = `0${hours}`;
-          }
-        let minutes = date.getMinutes();
-          if (minutes < 10) {
-             minutes = `0${minutes}`;
-          }
         let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         let day = days[date.getDay()];
 
-        return `${day} ${hours}:${minutes}`;
+        return `${day} ${formatHours(timestamp)}`;
       }
 
       function showTemp(response) { 
@@ -27,11 +19,58 @@
          document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
          document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
        }
+      
+      function formatHours (timestamp) {
+        let date = new Date(timestamp);
+        let hours = date.getHours();
+          if (hours < 10) {
+             hours = `0${hours}`;
+          }
+        let minutes = date.getMinutes();
+          if (minutes < 10) {
+             minutes = `0${minutes}`;
+          }
+        let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        let day = days[date.getDay()];
+
+
+        return `${hours}:${minutes}`;
+      }
+
+      function displayForecast(response) {
+        let forecastElement = document.querySelector("#forecast"); 
+        forecastElement.innerHTML = null;
+        let forecast = null;
+
+
+        for (let index = 0; index < 6; index++) {
+          forecast = response.data.list[index];
+          forecastElement.innerHTML += `
+           <div class="col-2">
+              <h3>
+                ${formatHours(forecast.dt * 1000)}
+              </h3>
+              <img
+               src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+               />
+             <div class="weather-forecast-temperature">
+                <strong>
+                ${Math.round(forecast.main.temp_max)}º
+                </strong>  
+                ${Math.round(forecast.main.temp_min)}º
+              </div>
+            </div>
+          `;
+        }
+      }
 
       function searchCity (city){
         let apiKey = "755fa0d585548b254a8058369f909e72";
         let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(showTemp);
+
+        apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayForecast);
        }
 
      function handleSubmit(event) {
@@ -59,7 +98,6 @@
        fahrenheitLink.classList.add("active");
        let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
        temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-       //remove the active class from celsius link
      }
 
      function displayCelsiusTemperature (event) {
